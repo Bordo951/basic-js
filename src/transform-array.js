4d35c6869c224378fff1b2f3be368a39a219ccf4
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-10-28 13:49:10 +0300

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  let copyArray = [];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!Array.isArray(arr)) {
+    throw new Error('arr is not an Array');
+  }
+
+  if (arr.length === 0) {
+    return copyArray;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-prev') {
+      if (i !== 0 && arr[i-2] !== '--discard-next') {
+        let prevValue = copyArray[copyArray.length-1];
+
+        copyArray.pop(prevValue);
+      }
+    } else if(arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0 && arr[i-2] !== '--discard-next') {
+        copyArray.push(arr[i-1])
+      }
+    } else if(arr[i] === '--double-next') {
+      if(i < arr.length-1) {
+        copyArray.push(arr[i+1])
+      }
+    } else {
+      copyArray.push(arr[i]);
+    }
+  }
+
+  return copyArray;
 };
