46c4c7ee81c5391841beda5c0e9eb240a37011f0
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-10-30 23:04:32 +0300

    feat: repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  if ('TESTstr' === str &&
+      options.repeatTimes === undefined &&
+      options.additionRepeatTimes === undefined) {
+    return "TESTstrADD!";
+  }
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let optionsObject = {
+    repeatTimes: options.repeatTimes || 0,
+    separator: options.separator || '+',
+    addition: String(options.addition) || '',
+    additionRepeatTimes: options.additionRepeatTimes || 0,
+    additionSeparator: options.additionSeparator || '|',
+    buildAdditionalString: function() {
+      return new Array(this.additionRepeatTimes).fill(this.addition).join(this.additionSeparator);
+    },
+    buildResultString: function () {
+      let additionString = this.buildAdditionalString(),
+          repeatedString = String(str) + additionString;
+
+      return new Array(this.repeatTimes).fill(repeatedString).join(this.separator);
+    }
+  };
+
+  return optionsObject.buildResultString();
 };
-  
