3a2b0c164caca96673d38d0b1998777c7b28e61f
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-10-30 21:27:48 +0300

    feat: add chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,62 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
+
+  /**
+   * Returns length chain
+   * @returns {number}
+   */
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+  /**
+   * Pushes value to chain
+   * @param {*} value
+   * @returns {Object}
+   */
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
+  /**
+   * Removes link by position
+   * @param {*} position
+   * @returns {Object}
+   */
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.isNoValidPosition(position)) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+  /**
+   * Reverses chain
+   * @returns {Object}
+   */
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+  /**
+   *Finalizes building chain
+   * @returns {string}
+   */
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.chain.map(function (element) {
+      return `( ${element} )`;
+    });
+    this.chain = [];
+    return chain.join('~~'); // Returns local variable 'chain'
+  },
+  /**
+   * Checks position for no valid values
+   * @param {*} position
+   * @returns {boolean}
+   */
+  isNoValidPosition(position) {
+    return position <= 0 || typeof position !== 'number' || position >= this.getLength();
   }
 };
 
